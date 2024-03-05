c6cf5d1c56216ee2606f8eb14423a96a434f2e08
Petoches <nicolas.dias.dasilva@gmail.com>
2024-03-05 14:48:36 +0100

    added readme, remove debug route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -18,15 +16,4 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Homepage');
-});
-
-Route::get('/debug', function() {
-
-    $response = Http::withHeaders([
-        'User-Agent' => config('services.instagram.user_agent'),
-        'x-ig-app-id' => config('services.instagram.x_ig_app_id')
-    ])->get('https://i.instagram.com/api/v1/users/web_profile_info/?username='.config('services.instagram.page'));
-
-
-    dd($response->object());
 });
